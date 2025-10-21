3aa6c8b8e3159117a1752eab695387946bb9e4ae
KateM3d <ekaterina8medvedeva@gmail.com>
2025-10-21 17:34:06 -0400

    add test data fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Profile } from './data/interfaces/profile.interface';
+import { ProfileService } from './data/services/profile.service';
 import { ProfileCardComponent } from './shared-components/profile-card/profile-card.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tik-talk';
+
+  profileService: ProfileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
 }
---
+++ src/app/data/services/profile.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Profile } from '../interfaces/profile.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  http: HttpClient = inject(HttpClient);
+  baseUrl = 'https://icherniakov.ru/yt-course/';
+  constructor() {}
+
+  getTestAccounts() {
+    return this.http.get<Profile[]>(`${this.baseUrl}account/test_accounts`);
+  }
+}
