6cd114e1e03737b3e7f4fbf4515553a407ae88fe
KateM3d <ekaterina8medvedeva@gmail.com>
2025-10-28 12:29:36 -0400

    add routing and sidebar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { Profile } from './data/interfaces/profile.interface';
-import { ProfileService } from './data/services/profile.service';
-import { ProfileCardComponent } from './shared-components/profile-card/profile-card.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProfileCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'tik-talk';
-
-  profileService: ProfileService = inject(ProfileService);
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe((val) => {
-      this.profiles = val;
-    });
-  }
-}
+export class AppComponent {}
---
+++ src/app/pages/search-page/search-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { ProfileService } from '../../data/services/profile.service';
+import { ProfileCardComponent } from '../../shared-components/profile-card/profile-card.component';
+
+@Component({
+  selector: 'app-search-page',
+  standalone: true,
+  imports: [ProfileCardComponent],
+  templateUrl: './search-page.component.html',
+  styleUrl: './search-page.component.scss',
+})
+export class SearchPageComponent {
+  title = 'tik-talk';
+
+  profileService: ProfileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
+}
